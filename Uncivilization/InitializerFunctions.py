Refined map select screen, added functionalities to Timer, begin player config file with changing screen size on startup. Fixed scaling of bordered hex assets, need to fix assets themselves (they are currently asymmetric).

Uncivilization/InitializerFunctions.py
@@ -10,8 +10,47 @@ from Uncivilization.HandleGameState import *
 from Uncivilization.HandleDraw import *
 from Uncivilization.IntroAnimation import *
 from Uncivilization.MapNameToInstructions import *
+from Uncivilization.Timer import Timer
+
 
 S3 = np.sqrt(3)
+SUPPORTED_RESOLUTIONS = sorted(
+    [
+        (720, 480),
+        (800, 600),
+        (1024, 768),
+        (1280, 720),
+        (1280, 800),
+        (1366, 768),
+        (1440, 900),
+        (1600, 900),
+        (1680, 1050),
+        (1920, 1080),
+        (1920, 1200),
+    ]
+)
+
+
+def closest_res(width, height):
+    if width < height:
+        print(f"{width} < {height}, swapping dimensions")
+        store = width
+        width = height
+        height = store
+
+    if (width, height) in SUPPORTED_RESOLUTIONS:
+        return width, height
+
+    r_user = width / height
+    r_supported = [w / h for w, h in SUPPORTED_RESOLUTIONS]
+    r_diffs = [abs(r_user - r) for r in r_supported]
+    min_diff = min(r_diffs)
+    i_first_min_diff = r_diffs.index(min_diff)
+    new_res = SUPPORTED_RESOLUTIONS[i_first_min_diff]
+    print(
+        f"{width} x {height} not supported!\nReformatting to smallest supported resolution which is closest by ratio: {new_res[0]} x {new_res[1]}"
+    )
+    return new_res
 
 
 def initialize_game_object(raw_assets, sounds, player_config):
@@ -23,14 +62,18 @@ def initialize_game_object(raw_assets, sounds, player_config):
     infoObject = pg.display.Info()
 
     # set default w/h
-    #width = 9 * infoObject.current_w // 10
-    #height = 9 * infoObject.current_h // 10
-
-    width = 15*infoObject.current_w//20
-    height = 15*infoObject.current_h//20
+    width = infoObject.current_w
+    height = infoObject.current_h
 
     # init screen
-    display = pg.display.set_mode((width, height), pg.SRCALPHA)
+    video_info = player_config["VIDEO DISPLAY OPTIONS"]["SCREEN_SIZE"]
+    video_info = video_info.lower().strip(" ").split("x")
+    video_info = video_info[0] if len(video_info) == 1 else (int(video_info[0]), int(video_info[1]))
+    width, height = video_info if len(video_info) == 2 else (width, height)
+    # video_flags = pg.SRCALPHA | pg.FULLSCREEN | pg.SCALED if video_info == "fullscreen" else pg.SRCALPHA
+    # display = pg.display.set_mode(size = (width, height), flags = video_flags)
+    width, height = closest_res(width, height)
+    display = pg.display.set_mode(size=(width, height))
 
     clock = pg.time.Clock()
 
@@ -40,23 +83,23 @@ def initialize_game_object(raw_assets, sounds, player_config):
     Game_Renderer = Renderer(display, camera, raw_assets)
     Game_State = GameState()
     Audio_Mixer = AudioMixer(sounds)
-    GAME = GameObject(Player_Input, Game_State, Game_Renderer,Audio_Mixer)
+    GAME = GameObject(Player_Input, Game_State, Game_Renderer, Audio_Mixer)
 
     # set game icon
     pg.display.set_icon(raw_assets["icon.png"])
-    menuSelector(GAME,clock)
+    menuSelector(GAME, clock)
 
 
-def menuSelector(game,clock):
+def menuSelector(game, clock):
     s = True
     gamestate = game.GameState
     while s:
         if gamestate.inMainMenu:
-            mainMenu(game,clock)
-        
+            mainMenu(game, clock)
+
         if gamestate.inMapSelect:
-            mapSelect(game,clock)
-        
+            mapSelect(game, clock)
+
         if gamestate.start_game:
             start_game(game, clock)
 
@@ -81,12 +124,15 @@ def start_game(game, clock):
         draw(game)
 
 
-def mapSelect(game,clock):
+def mapSelect(game, clock):
     t0 = time.time()
     gamestate = game.GameState
+    map_select_timer = Timer()
+    map_select_timer.start_timer()
+
     while gamestate.inMapSelect:
         clock.tick(game.MAX_FPS)
-        
+
         t1 = time.time()
         dt = t1 - t0
         t0 = t1
@@ -94,16 +140,17 @@ def mapSelect(game,clock):
 
         updateInputs(game)
         updateStateMapSelect(game)
-        drawMapSelect(game)
-    
-    if gamestate.start_game:
-        game.AudioMixer.stop_all()
+        drawMapSelect(game, map_select_timer)
 
-def mainMenu(game,clock):
+    if gamestate.start_game:
+        game.AudioMixer.fadeout_all()
+
+
+def mainMenu(game, clock):
     gamestate = game.GameState
 
     play_animation(game, clock)
-    
+
     # t0
     t0 = time.time()
     while gamestate.inMainMenu:

Uncivilization/MapNameToInstructions.py
@@ -1,34 +1,103 @@
 import pygame as pg
+import numpy as np
 import random
 import time
 
 from Uncivilization.Hex import *
 
-
-def convert_images(game):
+def convert_hexes(game):
     t0 = time.time()
 
     r = game.Renderer
     camera = r.camera
-
     mid_size = camera.hex_size
-    mid_scale = (
-        int(mid_size * S3) + r.hex_buff,
-        int(2 * mid_size) + r.hex_buff,
-    )
+
+    _ , twice_hex_size = r.default_hex_asset_size
+    mid_scale = get_bordered_hex_scale(game,twice_hex_size,mid_size,hex_buffer=r.default_hex_buff)
 
     r.assets = {
         img_name: pg.transform.scale(img.convert_alpha(), mid_scale)
         for img_name, img in r.assets.items()
     }
 
+    img = r.assets[list(r.assets.keys())[0]]
+    _,img_h = img.get_size()
+
+    r.current_hex_buff = (img_h - 2 * mid_size) / 2
+
     dt = time.time() - t0
-    dt = format(1000*dt,"0.2f")
+    dt = format(1000 * dt, "0.2f")
 
     print(f"Finished processing {len(r.assets.keys())} assets in {dt}ms")
 
+
+def get_random_dots_info(game, rect, rect_color, background_color, text_rect):
+    display = game.Renderer.display
+
+    x0, y0 = rect.topleft
+    x1, y1 = rect.bottomright
+
+    buffx = (x1 - x0) / 10
+    buffy = (y1 - y0) / 10
+
+    pg.draw.rect(display, background_color, rect)
+    pg.draw.rect(display, rect_color, rect)
+
+    dots_to_draw = np.random.randint(20, 30)
+    r = min(buffx, buffy) // 2
+
+    xtr, ytr = text_rect.topleft
+    xtr = xtr - r
+    ytr = ytr - r
+    wtr = text_rect.width + 2 * r
+    htr = text_rect.height + 2 * r
+    logical_rect = pg.rect.Rect((xtr, ytr), (wtr, htr))
+
+    redraw_list = []
+    for i in range(dots_to_draw):
+        color = random.choice(
+            [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 255), (255, 255, 0)]
+        )
+        rx = np.random.uniform(x0 + buffx, x1 - buffx)
+        ry = np.random.uniform(y0 + buffy, y1 - buffy)
+        while logical_rect.collidepoint((rx, ry)):
+            rx = np.random.uniform(x0 + buffx, x1 - buffx)
+            ry = np.random.uniform(y0 + buffy, y1 - buffy)
+        redraw_list.append([rx, ry, color, r])
+    return redraw_list
+
+
+def draw_random_dots(game, rect, rect_color, timer, background_color=(0, 0, 0)):
+    rend = game.Renderer
+    display = rend.display
+
+    title_string = "Random"
+    largeText = rend.largeText
+    TextSurf = largeText.render(title_string, True, (0, 0, 0))
+    text_rect = TextSurf.get_rect(center=rect.center)
+
+    if rend.mapSelectRedraw is None:
+        game.Renderer.mapSelectRedraw = get_random_dots_info(
+            game, rect, rect_color, background_color, text_rect
+        )
+
+    if timer.smart_peek_timer() > 0.333333:
+        timer.end_timer()
+        game.Renderer.mapSelectRedraw = get_random_dots_info(
+            game, rect, rect_color, background_color, text_rect
+        )
+        timer.start_timer()
+
+    to_draw = rend.mapSelectRedraw
+    for draw_info in to_draw:
+        rx, ry, color, r = draw_info
+        pg.draw.circle(display, color, (rx, ry), r)
+
+    display.blit(TextSurf, text_rect)
+
+
 def random_tiles(game):
-    convert_images(game)
+    convert_hexes(game)
 
     state = game.GameState
     grid = state.grid_size
@@ -56,6 +125,6 @@ def random_tiles(game):
             state.board.update({f"{q},{r}": tile})
 
 
-MAP_TO_FUNC = {
-    "random": random_tiles
-    }
+MAP_TO_FUNC = {"random": random_tiles}
+
+MAP_TO_DISPLAY = {"random": draw_random_dots}